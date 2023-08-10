2590f6145b67c06c4d88d65adb60568a2798b14b
maratahmetov <ax-marat@mail.ru>
2023-08-10 03:33:46 +0300

    =edit url

--- CourseSell20/source/urls.py
+++ CourseSell20/source/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from source import views
 from django.conf import settings
 from django.conf.urls.static import static
-from source.views import GameListView, ProfileView, AddGameView
+from source.views import GameListView, ProfileView, AddGameView, BalanceView, CartListView
 
 urlpatterns = [
     path("", GameListView.as_view(), name="game_list"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("addgame/", AddGameView.as_view(), name="addgame"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('balance/', BalanceView.as_view(), name='balance'),
+    path('cart/', CartListView.as_view(), name='cart'),
+
+]

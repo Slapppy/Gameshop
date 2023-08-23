eaf8d6d1a8db3f1d7ac5d86c0785ec4f5cb5fe6d
maratahmetov <ax-marat@mail.ru>
2023-08-23 19:17:30 +0300

    +smtp

--- CourseSell20/CourseSell20/settings.py
+++ CourseSell20/CourseSell20/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "api",
     "CustomAuth",
     "source",
-
 ]
 
 
@@ -151,3 +150,10 @@ SIMPLE_JWT = {
 
 
 AUTH_USER_MODEL = "source.User"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "slappyyya@gmail.com"
+EMAIL_HOST_PASSWORD = "w0PjvcQEKg8rLTD1"
